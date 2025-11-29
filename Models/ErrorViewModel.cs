052580932a731241aa888ebb58480140ef00eeb4
MustafaEmre_halli <mustafa.halli@ogr.sakarya.edu.tr>
2025-11-29 21:30:25 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TufanOto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
